8f346a43f20c9638cc98c0dfc80817a1e227fe4c
Nur Mohammad Rashed <rashed@nordfincapital.com>
2023-10-13 10:54:12 +0200

    feat: navbar completed

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,25 @@
+import Logo from '../../assets/logo.png'
 import "./navbar.css";
+import data from './data';
+import { IoIosColorPalette } from "react-icons/io";
 const NavBar = () => {
   return (
     <nav>
-      <h2>Navbar</h2>
+      <div className="container nav__container">
+        <a href="index.html" className="nav__logo">
+          <img src={Logo} alt="Logo" />
+        </a>
+        <ul className="nav__menu">
+          {data.map((item) => (
+            <li key={item.id}>
+              <a href={item.link}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+        <button id="theme__icon">
+          <IoIosColorPalette />
+        </button>
+      </div>
     </nav>
   );
 };
