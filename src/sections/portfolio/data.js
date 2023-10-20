65cd72ce98ba4942fe10d260b95121b3b3c31e24
Nur Mohammad Rashed <rashed@nordfincapital.com>
2023-10-20 18:30:41 +0200

    feat:portfolio is done

---
+++ src/sections/portfolio/data.js
@@ -0,0 +1,95 @@
+import Image1 from "../../assets/Sass project.png";
+import Image2 from "../../assets/PigGame.png";
+import Image3 from "../../assets/Starwars.png";
+import Image4 from "../../assets/BudgetApp.png";
+import Image5 from "../../assets/GuessMyNumber.png";
+import Image6 from "../../assets/MovieSearch.png";
+import Image7 from "../../assets/BioBIo.png";
+import Image8 from "../../assets/Bank.png";
+import Image9 from "../../assets/KidsSchool.png";
+
+const data = [
+  {
+    id: 1,
+    category: "uiux",
+    image: Image1,
+    title: "Project Title One (UIUX)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    demo: "https://outdoorwebsite.netlify.app/",
+    github: "https://github.com/nurrashed/SCSS-Project-1",
+  },
+  {
+    id: 2,
+    category: "frontend",
+    image: Image2,
+    title: "Project Title Two (Frontend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    demo: "https://rashed-pig-game.netlify.app/",
+    github: "https://github.com/nurrashed/Pig-Game",
+  },
+  {
+    id: 3,
+    category: "frontend",
+    image: Image3,
+    title: "Project Title Three (Frontend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    demo: "https://my-star-wars-app.netlify.app/",
+    github: "https://github.com/nurrashed/starwar-app-node",
+  },
+  {
+    id: 4,
+    category: "uiux",
+    image: Image4,
+    title: "Project Title Four (UIUX)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    demo: "https://budget-app-rashed.netlify.app/",
+    github: "https://github.com/nurrashed/BudgetApp-JavaScript",
+  },
+  {
+    id: 5,
+    category: "uiux",
+    image: Image5,
+    title: "Project Title Five (UIUX)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! ",
+    demo: "https://guess-my-number-app.netlify.app/",
+    github: "https://github.com/nurrashed/Guess-my-number-game",
+  },
+  {
+    id: 6,
+    category: "frontend",
+    image: Image6,
+    title: "Project Title Six (Frontend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    demo: "https://mymoviesearchapp.netlify.app/",
+    github: "https://github.com/nurrashed/MovieDatabase",
+  },
+  {
+    id: 7,
+    category: "frontend",
+    image: Image7,
+    title: "Project Title Seven (Frontend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    demo: "https://mybiobio.netlify.app/",
+    github: "https://github.com/nurrashed/BioBio",
+  },
+  {
+    id: 8,
+    category: "backend",
+    image: Image8,
+    title: "Project Title Eight (Backend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    demo: "https://my-banking-site.netlify.app/",
+    github: "https://github.com/nurrashed/Banking",
+  },
+  {
+    id: 9,
+    category: "frontend",
+    image: Image9,
+    title: "Project Title Nine (Frontend)",
+    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+    demo: "https://kidzschool-css-html-javascript.netlify.app/",
+    github: "https://github.com/nurrashed/KidzSchoolwebsite-HTML-CSS-JavaScript",
+  },
+];
+
+export default data;
