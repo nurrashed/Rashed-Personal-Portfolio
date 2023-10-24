a6a38fbc6f9097e714828b015f01dedd9a194a18
Nur Mohammad Rashed <rashed@nordfincapital.com>
2023-10-24 18:54:10 +0200

    feat:testimonials complete

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,7 +1,49 @@
-import './testimonials.css'
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay"
+
+// import required modules
+import { Pagination, Autoplay, Keyboard, } from "swiper/modules";
+
+import testimonials from "./data";
+import Testimonial from "./Testimonial";
+import "./testimonials.css";
 
 const Testimonials = () => {
-  return <section id="testimonials"><h2>Testimonials</h2></section>;
+  return (
+    <section id="testimonials">
+      <h2>What My Colleagues Say about me</h2>
+      <p>These are unbiased testmonials from some of my Colleagues</p>
+      <div className="container">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          breakpoints={{
+            601: { slidesPerView: 2 },
+            1025: { slidesPerView: 3 },
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          mousewheel={true}
+          keyboard={{
+            enabled: true,
+          }}
+          modules={[Pagination, Autoplay, Keyboard]}
+          className="mySwiper"
+        >
+          {testimonials.map((testimonial) => (
+            <SwiperSlide key={testimonial.id}>
+              <Testimonial testimonial={testimonial} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
---
+++ src/sections/testimonials/data.js
@@ -0,0 +1,64 @@
+import Rizvi from "../../assets/Rizvi.jpg";
+import Muthu from "../../assets/Muthu.jpg";
+import Aadil from "../../assets/Aadil.jpg";
+import Adam from "../../assets/Adam.jpg";
+const data = [
+  {
+    id: 1,
+    quote1: `I have managed Nur Mohammad Rashed directly at Nordfin Capital. 
+        What impressed me about Nur Mohammad, is his ability to adapt with the fast changing business needs.
+        I found him very enthusiastic about Technology and modern software development practices.
+        He was open to receive constructive feedback and very dedicated to his role.`,
+
+    quote2: `I believe any organization will benefit from having Nur Mohammad Rashed in their software development team.`,
+    avatar: Rizvi,
+    name: "Sarwarul Rizvi",
+    profession: "Engineering Manager at Keystone Education Group",
+  },
+  {
+    id: 2,
+    quote1: `Rashed is a really hardworking person who is always prepared to take on any challenge that comes 
+    his way. It is very remarkable how committed he is to his career. He continuously goes above and beyond to 
+    achieve successful project delivery.He exhibits uncompromising dedication to his tasks. Rashed's dedication 
+    to keeping deadlines is admirable. He was important in helping us finish it on time while working together 
+    to migrate our application to React. `,
+    quote2: `His outstanding performance and friendly attitude make a big 
+    impression on the team. In addition to his technical abilities, Rashed is a valuable and well-liked 
+    team member due to his positive personality and cheerful attitude. Rashed is a valuable addition to any
+    team because of his strong work ethic, commitment, and kind approach. I strongly agree.`,
+    avatar: Muthu,
+    name: "Muthusamy Ramasamy",
+    profession: "Tech Lead at Nordfin Capital Group",
+  },
+  {
+    id: 3,
+    quote1: `I had the pleasure of collaborating closely with Rashed at Nordfin, even though we were on 
+    different teams. I highly recommend him for any frontend development role. He has an exceptional 
+    ability to understand complex concepts effortlessly, he brings invaluable insights to projects. 
+    Rashed's tireless work ethic and unwavering commitment make him a reliable and results-oriented professional.`,
+    quote2: `He has a remarkable thirst for knowledge, eager for new challenges and a quick learner. 
+    This adaptability, combined with his strong problem-solving skills, ensures consistent results.
+    Rashed fosters an inspiring and collaborative work environment and motivates colleagues to succeed.
+    His passion for growth and excellence makes him a real asset to any team.
+    I recommend Rashed for any frontend development position.`,
+    avatar: Aadil,
+    name: "Muhammad Aadil",
+    profession: "Software Developer at Nordfin Capital Group",
+  },
+  {
+    id: 4,
+    quote1: `When Nur joined our team of react native developers, he showed a dedicated passion to excel and 
+    grow with a frame-work he had no prior experience of. Nonetheless he delved right into online
+    classes and maintained a strict schedule, until he was able to quickly go from debugging to
+    expanding with advanced new feature.`,
+    quote2: `He was understanding of the difficulties with
+    working during a pandemic yet always came with a cheerful attitude and in our remote
+    stand ups always finished the meetings with "I can do it". I thus give my highest recommendation
+    and wish Nur the best of luck.`,
+    avatar: Adam,
+    name: "Adam Sam",
+    profession: "CEO at Monok.com - AI Startup within Content Generation",
+  },
+];
+
+export default data;
