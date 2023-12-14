677d100637d84aa711560b5d57e27bc8c5d76e44
Nur Mohammad Rashed <rashed@nordfincapital.com>
2023-12-14 11:30:49 +0100

    fix: about section fix

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,9 +1,10 @@
-import AboutImage from '../../assets/about1.jpg';
-import CV from '../../assets/CV_Nur Mohammad Rashed_English.pdf';
+import AboutImage from "../../assets/about1.jpg";
+import CV from "../../assets/CV_Nur Mohammad Rashed.pdf";
+import CoverLetter from "../../assets/Cover_Letter_Nur Mohammad Rashed.pdf";
 import { HiDownload } from "react-icons/hi";
-import Card from '../../components/Card';
-import data from './data';
-import './about.css';
+import Card from "../../components/Card";
+import data from "./data";
+import "./about.css";
 
 const About = () => {
   return (
@@ -25,22 +26,68 @@ const About = () => {
               </Card>
             ))}
           </div>
-          <p>
-            Building projects my clients love have always been my passion. Being
-            in the web development industry for over 3 years and serving more
-            than 70 happy clients worldwide, I'm always motivated to do more!
+          <p style={{ textAlign: "justify" }}>
+            With over two and half years of professional experience as a
+            front-end developer, I have led and managed various front-end
+            projects from scratch. Throughout my career, I have had the
+            opportunity to work with various projects in the Fintech Industry
+            and gained extensive experience in crafting responsive and
+            user-friendly web interfaces. I am good at HTML, CSS, JavaScript,
+            TypeScript, and I'm also proficient in frontend frameworks,
+            including React, Next.js, Angular and Vue.JS.
           </p>
-          <p>
-            Hi, my name is Hajia Bintu from Accra, Ghana. I'm a full-stack web
-            developer with a Bachelors degree in Computer Science. My top
-            priority is to get your business online the right way, giving you
-            industry-standard design and all the functionality you need to
-            operate smoothly online. Get in touch today with the details of your
-            project let's get started! Check out my resume below!
+          <p style={{ textAlign: "justify" }}>
+            As a front-end developer for Nordfin Capital Group, I created an
+            invoice management platform that enables users to choose different
+            payment options and access other features. The platform serves
+            thousands of customers and provides satisfactory invoicing service
+            for Nordfin Capital Group. I also produced, maintained, and modified
+            the company's internal websites and user interfaces, and worked with
+            automated testing and bug fixing. Moreover I have experience working
+            in agile development environment and collaborating with
+            cross-functional teams.
           </p>
-          <a href={CV} download className="btn primary">
-            Download CV <HiDownload />
-          </a>
+          <p style={{ textAlign: "justify" }}>
+            A few of my key strengths are finding unique and simple solutions to
+            problems that arise while ensuring a good customer experience, the
+            ability to work independently from any stage of a project’s life
+            cycle and performing well in a fast-paced environment. I consider
+            myself a team player who can independently find different ways to
+            move forward as well.
+          </p>
+          <p style={{ textAlign: "justify" }}>
+            Outside work, I love to explore new places and I consider myself a
+            film enthusiast. These hobbies boost my wellbeing, reduce stress and
+            burnout.
+          </p>
+          <p style={{ textAlign: "justify" }}>
+            If you would like to discuss any front-end job opportunity, any kind
+            of collaboration or even your favourite destination or film, feel
+            free to email me @ rashed_huawei@yahoo.com{" "}
+          </p>
+          <p style={{ textAlign: "justify" }}>
+            If you would like to discuss any front-end job opportunity, any kind
+            of collaboration or even your favourite destination or film, feel
+            free to email me @ rashed_huawei@yahoo.com
+          </p>
+          <p style={{ textAlign: "justify" }}>
+            Skills: JavaScript, React, TypeScript, RestAPI, Unit Testing, UX/UI,
+            HTML, CSS, Tailwind CSS, Bootstrap, SCSS, Material UI, Vite.JS,
+            Next.JS, Angular, Vue, C#, .Net, Node, MongoDB, Docker, Kubernetes,
+            Git, Azure DevOps, CI/CD
+          </p>
+          <div className="downloadCV">
+            <a href={CV} download className="btn primary cvButton">
+              Download CV <HiDownload />
+            </a>
+            <a
+              href={CoverLetter}
+              download
+              className="btn primary coverLetterButton"
+            >
+              Download Cover Letter <HiDownload />
+            </a>
+          </div>
         </div>
       </div>
     </section>
